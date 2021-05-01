60fc444bb59575fa89c5561a29ab46b3e5e9e022
Tahsin <tahsingenuies@gmail.com>
2021-05-02 05:18:32 +0700

    include data from controller

---
+++ src/controllers/api/data/sendBasicData.ts
@@ -0,0 +1,22 @@
+import { BaseController } from '@src/controllers/api/baseController'
+import { Request, Response } from 'express'
+import Joi from 'joi'
+import AppData from '@root/src/repositories/AppData'
+
+class Controller extends BaseController {
+  requestValidationSchema = {
+    body: Joi.object({}).required(),
+    query: Joi.object({}).required(),
+    header: Joi.object({}).required(),
+  }
+
+  requestHandler = async (req: Request, res: Response) => {
+    await this.validateRequest(req)
+
+    const data = await AppData.get()
+
+    res.status(200).json(data)
+  }
+}
+
+export default new Controller()
