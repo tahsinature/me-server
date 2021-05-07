b466075b72a89e0cea6b42b34579fbf33d5a8812
Tahsin <tahsingenuies@gmail.com>
2021-05-07 16:02:52 +0700

    send uptime on helth check

--- src/controllers/api/health/getAppStatus.ts
+++ src/controllers/api/health/getAppStatus.ts
@@ -1,11 +1,17 @@
 import { BaseController } from '@src/controllers/api/baseController'
 import { Request, Response } from 'express'
+import moment from 'moment'
 
 class Controller extends BaseController {
   requestValidationSchema = {}
 
   requestHandler = async (req: Request, res: Response) => {
-    this.sendResponse(req, res, { data: { status: 'OK' } })
+    this.sendResponse(req, res, {
+      data: {
+        status: 'OK',
+        upTime: moment.duration(process.uptime(), 'second').humanize(),
+      },
+    })
   }
 }
 
