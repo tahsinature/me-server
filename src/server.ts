19ebdad6feb84524d7bcf9fb03254a10bb822454
Tahsin <tahsingenuies@gmail.com>
2021-05-15 04:45:17 +0700

    refresh socket ids in db on app start

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import Container from './container'
 import logger from './logger'
-import { initSocket } from '@src/socket'
 import http from 'http'
+import socket from '@root/src/socket'
+import { initSocketRouts } from '@root/src/router'
+import { repositories } from '@root/src/repositories'
 
 class Server {
   constructor(private container: Container) {
@@ -15,12 +17,12 @@ class Server {
     const port = process.env.PORT || 3001
     const server = new http.Server(this.container.app).listen(port)
 
-    initSocket(server)
+    const io = await socket.initSocket(server)
+    await initSocketRouts(io)
+    await repositories.connection.refreshSocketIds()
 
-    server.on('listening', () => {
-      console.log('\x1b[36m%s\x1b[0m', `🌏 Express server started at http://localhost:${port}`)
-      console.log('\x1b[36m%s\x1b[0m', `⚙️  Swagger UI hosted at http://localhost:${port}/api-docs`)
-    })
+    console.log('\x1b[36m%s\x1b[0m', `🌏 Express server started at http://localhost:${port}`)
+    console.log('\x1b[36m%s\x1b[0m', `⚙️  Swagger UI hosted at http://localhost:${port}/api-docs`)
   }
 }
 
