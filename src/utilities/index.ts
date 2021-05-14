5391c6892ce33423e508793e6ad0567220d3636f
Tahsin <tahsingenuies@gmail.com>
2021-05-15 04:44:25 +0700

    add new utility fn

---
+++ src/utilities/index.ts
@@ -0,0 +1,9 @@
+import wait from '@root/src/utilities/wait'
+import sendResponse from '@root/src/utilities/sendResponse'
+
+const utilities = {
+  wait,
+  sendResponse,
+}
+
+export default utilities
