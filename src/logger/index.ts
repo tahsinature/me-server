416f94fc71c73a1b195cafbb83a75d45fd3390e8
Tahsin <tahsingenuies@gmail.com>
2021-05-30 01:18:01 +0700

    create elk logger

--- src/logger/index.ts
+++ src/logger/index.ts
@@ -1,29 +1,17 @@
 import { createLogger, format, transports } from 'winston'
-
-const consoleTransport = new transports.Console({
-  level: 'debug',
-  format: format.prettyPrint(),
-})
-
-const fileTransport = new transports.File({
-  level: 'error',
-  filename: './logs/error.log',
-  format: format.json({
-    replacer: (key, value) => {
-      if (key === 'error') {
-        return {
-          message: (value as Error).message,
-          stack: (value as Error).stack,
-        }
-      }
-      return value
-    },
-  }),
-})
+import fileTransport from '@root/src/logger/transports/fileTransport'
+import consoleTransport from '@root/src/logger/transports/consoleTransport'
+import elkTransport from '@root/src/logger/transports/elkTransport'
 
 const logger = createLogger({
   format: format.combine(format.timestamp()),
   transports: [fileTransport, ...(process.env.NODE_ENV === 'development' ? [consoleTransport] : [])],
+  defaultMeta: { service: 'api' },
+})
+
+export const httpLogger = createLogger({
+  format: format.combine(format.timestamp()),
+  transports: [elkTransport],
   defaultMeta: { service: 'api' },
 })
 
