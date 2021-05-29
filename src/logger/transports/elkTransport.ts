83714a9c396096c3e10ac86ad97ec07f8303512e
Tahsin <tahsingenuies@gmail.com>
2021-05-30 01:25:51 +0700

    mv cred to env

--- src/logger/transports/elkTransport.ts
+++ src/logger/transports/elkTransport.ts
@@ -1,7 +1,7 @@
 import { ElasticsearchTransport } from 'winston-elasticsearch'
 
 const elkTransport = new ElasticsearchTransport({
-  clientOpts: { node: 'https://wuia3ursgh:3f2wx4n3rc@me-server-8414750834.us-east-1.bonsaisearch.net:443' },
+  clientOpts: { node: process.env.ELASTICSEARCH_HOST },
   index: `logs-${'http'}`,
 })
 
